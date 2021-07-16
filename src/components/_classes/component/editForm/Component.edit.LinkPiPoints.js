de4687d9434df952d769147f0bba74534452270c
Marc Lytle <marc.lytle@jsatautomation.com>
2021-07-16 13:57:47 -0700

    added Input Type to modal

--- src/components/_classes/component/editForm/Component.edit.LinkPiPoints.js
+++ src/components/_classes/component/editForm/Component.edit.LinkPiPoints.js
@@ -30,7 +30,35 @@ export default [
           { label: 'Point3', value: 'point_3' },
           { label: 'Point4', value: 'point_4' }
         );
-        context.utils.eachComponent(context.instance.options.editForm.components, function(component, path) {
+        context.utils.eachComponent(context.instance.options.editForm.components, function (component, path) {
+          if (component.key !== context.data.key) {
+            values.push({
+              label: component.label || component.key,
+              value: path
+            });
+          }
+        });
+        return values;
+      }
+    },
+  },
+  {
+    type: 'select',
+    input: true,
+    key: 'inputType',
+    label: 'Input Type',
+    weight: 20,
+    tooltip: 'The Type of Input required.',
+    dataSrc: 'custom',
+    valueProperty: 'value',
+    data: {
+      custom(context) {
+        var values = [];
+        values.push(
+          { label: 'Textfield', value: '1' },
+          { label: 'Number', value: '2' }
+        );
+        context.utils.eachComponent(context.instance.options.editForm.components, function (component, path) {
           if (component.key !== context.data.key) {
             values.push({
               label: component.label || component.key,
